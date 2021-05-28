953508521c83d3a912b6b19882a8bb171d5bf07f
George Agriogiannis <george.agriogiannis@suse.com>
2021-05-28 20:15:01 +0200

    working state

--- docker_build_script/image-builder.rb
+++ docker_build_script/image-builder.rb
@@ -1,13 +1,5 @@
-#!/usr/bin/ruby
+#!/usr/bin/ruby -w
 # Add comments explaining how it works.
-
-# Add check for first argument,use that for image name, else ask for image name
-puts 'Please give the image name'
-read $image
-
-# Add check for second argument, use that for tag name, else ask for tag name
-puts 'Please give the tag name'
-read $tag
 
 puts 'Step 1: Checking SCCcredentials'
 if(File.exist?('SCCcredentials'))
@@ -25,12 +17,22 @@ else
    exit
 end
 
+# Check number of arguments (should always be two)
+if ARGV.length != 2
+  puts "We need exactly two arguments"
+  exit
+end
+
+image = ARGV[0]
+tag = ARGV[1]
+
 puts 'Step 3: Checking BUILDKIT syntax requirements'
 line1 = "# syntax = docker/dockerfile:experimental"
 line1.freeze
-syntax = File.open(Dockerfile).1
-if line1 == syntax
-# FIX image, tag and command
-   BUILDKIT=1 docker image create --pull --id=SCCcredentials $image:$tag .
+syntax = File.open("Dockerfile").first.chomp
+if line1 .eql? syntax
+  command = "DOCKER_BUILDKIT=1 docker build --no-cache --pull --secret id=SCCcredentials,src=SCCcredentials -t #{image}:#{tag} ."
+  exec command
 else
-  puts 'Not correct Dockerfile syntax, please add'+line1+'at the top of the file'
+  puts 'Not correct Dockerfile syntax, please add "'+line1+'" at the top of the file'
+end
